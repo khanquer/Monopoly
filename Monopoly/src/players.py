e045c5b515bf70d9c630e4225fd921094dee6a64
khanquer <shafar03@gmail.com>
2018-12-29 18:50:34 -0500

    Updated the adding of props to Prop Lists

--- Monopoly/src/players.py
+++ Monopoly/src/players.py
@@ -3,6 +3,8 @@ class Players(object):
 		self.num = num
 		self.loc = loc
 		self.propList = pL
+		self.money = 1500
 	def __str__(self):
-		return ( "num = " + str(self.num) + " | loc = "  + str(self.loc)+ " | propList = " + str(self.propList))
+		#return ( "num = " + str(self.num) + " | loc = "  + str(self.loc)+ " | propList = " + str(self.propList))
+		return ( str(self.num) + " | loc = "  + str(self.loc)+ " | $ = " + str(self.money) + " | propList = " + str(self.propList))
 
